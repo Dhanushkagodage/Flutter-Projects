286b1833d6f4e0f9395f02cb379a20070969279d
Godagesasindu <godagesasindu@gmail.com>
2024-06-01 11:03:31 +0530

    First page Added

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,86 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black,
           appBar: AppBar(
             backgroundColor: Colors.black,
-            shape: const BorderDirectional(bottom: BorderSide(color: Color.fromARGB(115, 228, 225, 225))),
-            title:const Text(
+            shape: const BorderDirectional(
+                bottom: BorderSide(color: Color.fromARGB(115, 228, 225, 225))),
+            title: const Text(
               "BACK HOLE",
               style: TextStyle(
-                        fontWeight:FontWeight.w900,
-                        fontSize:30,
-                        color: Colors.white),
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Colors.white),
             ),
             actions: [
-              IconButton(onPressed: (){}, icon: const Icon(Icons.menu))
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+                color: Colors.white,
+                iconSize: 30,
+              )
             ],
           ),
-          
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "SPACE DETAILS",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: Image.asset(
+                      "assets/space1.png",
+                      width: 238,
+                      height: 237,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: Container(
+                      width:231 ,
+                      height:47 ,
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.red,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "SPACE DETAILS",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
